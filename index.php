585b2bdf3e0fa0bc895f7800afeb50fec6af9386
罗江涛 <1368761119@qq.com>
2018-01-17 11:29:29 +0800

    bug

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     header('Content-type:text/html;charset=utf-8');
     use aaphp\AA;
-//var_dump($_SERVER);die;
+var_dump($_SERVER);die;
 	// 调试模式开启
 	// define("DEBUG", false);
 	define("DEBUG", true);
